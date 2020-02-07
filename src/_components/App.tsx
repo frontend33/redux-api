d11ec3413963c80d4efcead4f7b0ac1058a107d3
Викулов Георгий Александрович <vikulov.georgiy@wildberries.ru>
2020-02-07 12:23:01 +0300

    add redux

---
+++ src/_components/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>redux</div>;
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './_components/App'
+// Импортим функции которые возвращают action которые отправляются в редьюсер
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './_store/actions'
+import todoApp from './_store/reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todoApp)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Выведем в консоль начальное состояние
+console.log(store.getState())
+
+
+
+// Каждый раз при обновлении состояния - выводим его
+// Отметим, что subscribe() возвращает функцию для отмены регистрации слушателя
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// Отправим несколько экшенов
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Прекратим слушать обновление состояния
+unsubscribe()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
